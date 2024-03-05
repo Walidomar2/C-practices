fd47ac6657abdb0526201dad6daf8f5f01e752fc
Walid Omar <walidomar291@gmail.com>
2024-03-05 12:40:19 +0200

    web urls history with stack

---
+++ WebHistory_stack/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            Stack<Url> undo = new Stack<Url>();
+            Stack<Url> redo = new Stack<Url>();
+
+            string line = string.Empty;
+            while(true)
+            {
+                Console.Write("Enter Url  or ('exit' to quit): ");
+                line = Console.ReadLine().ToLower();
+
+                if (line == "exit")
+                {
+                    break;
+                }
+                else if (line == "back")
+                {
+                    if (undo.Count > 0)
+                    {
+                        var url = undo.Pop();
+                        redo.Push(url);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (line == "forward")
+                {
+                    if (redo.Count > 0)
+                    {
+                        var url = redo.Pop();
+                        undo.Push(url);
+                    }
+                    else
+                    {
+                        continue; 
+                    }
+                }
+                else
+                {
+                    undo.Push(new Url(line));
+                }
+                
+                Console.Clear();
+
+                PrintStack("Back",undo);
+                PrintStack("Forward", redo);
+            }
+        }
+
+        static void PrintStack(string name, Stack<Url> urls)
+        {
+            Console.WriteLine($"{name} history");
+            Console.BackgroundColor = name.ToLower() == "back" ? ConsoleColor.DarkGreen : ConsoleColor.DarkBlue;
+            foreach (var u in urls)
+            {
+                Console.WriteLine($"\t{u}");
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
---
+++ WebHistory_stack/Url.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace App
+{
+    public class Url
+    {
+        private readonly DateTime _createdAt;
+        private readonly string _url = string.Empty;
+
+        public Url(string url)
+        {
+            _createdAt = DateTime.Now;
+            _url = url;
+        }
+
+        public override string ToString()
+        {
+            return $"[{this._createdAt.ToString("yyyy-MM-dd hh:mm")}] {this._url}";
+        }
+    }
+}
