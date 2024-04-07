33dfc130d52d3f4ea0b5436c700c41c74fbb85ee
Walid Omar <walidomar291@gmail.com>
2024-04-07 16:20:25 +0200

    plindrome words problem

---
+++ plindrome_words/plindrome_words/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Enter your Word: ");
+                string? word = Console.ReadLine();
+               
+
+                bool isPlindrome = true;
+
+                for(int i = 0; i < word.Length/2; i++)
+                {
+                    if(char.ToLower(word[i]) != char.ToLower(word[word.Length - i - 1]))
+                    {
+                        isPlindrome = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("Word is " + (isPlindrome ? "palindrome" : "not palindrome"));
+            }
+
+        }
+    }
+}
